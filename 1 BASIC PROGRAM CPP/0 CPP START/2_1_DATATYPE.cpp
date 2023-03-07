2021d0f068ad78ac71d5881f95250e0e54c7ddf0
Mayur patil <mayurpatil7603@gmail.com>
2023-03-07 07:49:39 +0530

    some updates

--- 1 BASIC PROGRAM CPP/0 CPP START/2_1_DATATYPE.cpp
+++ 1 BASIC PROGRAM CPP/0 CPP START/2_1_DATATYPE.cpp
@@ -54,4 +54,15 @@ int main()
     return 0;
 }
 
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  bool x = true ;
+  bool y = false ;
+  cout << x << endl;    //x and y need to be cout on separate lines
+  cout << y ;
+
+  return 0;
+}
 */
